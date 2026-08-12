b673603560daa238b60c7fca73b5e3c1e8f74a61
pss-aditya <adityavalekar2277@gmail.com>
2026-08-12 20:22:34 +0530

    Add basic code for now

---
+++ Program2.java
@@ -0,0 +1,20 @@
+///////////////////////////////
+/// Program to Add to Number
+///////////////////////////////
+
+/////////////////////////////////////
+/// Not a Good way to write the code 
+/////////////////////////////////////
+public class Program2{
+    public static void main(String Args[])
+    {
+        int A = 10;
+        int B = 20;
+        int J = 0;
+
+        J = A + B ;
+        System.out.println("The output is :" + J);
+
+    }
+
+}
