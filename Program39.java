268dd3f41c9f449fada3db0aa077529814f49ec6
pss-aditya <adityavalekar2277@gmail.com>
2026-08-28 23:23:33 +0530

    29-08-2026

--- Program39.java
+++ Program39.java
@@ -1,3 +1,42 @@
-public class Program39 {
-    
+import java.util.Scanner;
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+// Program : Accept the radius of a circle from the user and calculate its area
+//           by considering the value of PI as 3.14.
+//
+// Example 1 :
+// Input : 5.3
+// Output: 88.2026
+//
+// Example 2 :
+// Input : 10.4
+// Output: 339.6224
+//
+// Concept : OOP
+//
+// Date : 28/08/2026
+// Author : Aditya Govind Valekar
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+public class Program39
+{
+    public static void main(String[] args)
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        double radius = 0.0;
+        double area = 0.0;
+        final double PI = 3.14;
+
+        System.out.println("Enter the radius of circle :");
+        radius = sobj.nextDouble();
+
+        area = PI * radius * radius;
+
+        System.out.println("Area of circle is : " + area);
+
+        sobj.close();
+    }
 }
--- Program40.java
+++ Program40.java
@@ -1,3 +1,47 @@
-public class Program40 {
-    
+import java.util.Scanner;
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+// Program : Accept the width and height of a rectangle from the user and calculate
+//           its area.
+//
+// Example :
+// Input : 5.3 9.78
+// Output: 51.834
+//
+// Concept : Procedural Programming
+//
+// Date : 28/08/2026
+//
+// Author : Aditya Govind Valekar
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+class Program40
+{
+    public static double CalculateArea(double width, double height)
+    {
+        return width * height;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        double width = 0.0;
+        double height = 0.0;
+        double area = 0.0;
+
+        System.out.println("Enter width : ");
+        width = sobj.nextDouble();
+
+        System.out.println("Enter height : ");
+        height = sobj.nextDouble();
+
+        area = CalculateArea(width, height);
+
+        System.out.println("Area of rectangle is : " + area);
+
+        sobj.close();
+    }
 }
